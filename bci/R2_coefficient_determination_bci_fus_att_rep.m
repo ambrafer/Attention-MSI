de73500fb12c7bfff604c89e15c1f62b3dbb9a1b
ambrafer <34576307+ambrafer@users.noreply.github.com>
2021-10-19 11:12:23 +0200

    Add files via upload

---
+++ bci/R2_coefficient_determination_bci_fus_att_rep.m
@@ -0,0 +1,168 @@
+%% Compute R^2
+
+clear;
+close all;
+clc;
+
+% Path to BCI scripts
+addpath(genpath('E:\AMBRA\UoB\Exp\misc'));
+
+expID = 'MRI'; % MRI, behav, behav_MRI
+
+if strcmp(expID, 'MRI')
+    cd('E:\AMBRA\UoB\Data\MAMSI_MRI\group\behav\BCI\single_fit');
+    % Subjects
+    subjID = {'sub-MA01';'sub-MA08';'sub-MA53';'sub-MA98';'sub-MA114';'sub-MA117';...
+        'sub-MA124';'sub-MA129';'sub-MA130';'sub-MA132';'sub-MA138';'sub-MA145'};
+elseif  strcmp(expID, 'behav')
+    cd('E:\AMBRA\UoB\Data\MAMSI_MRI\group\behav\screening\BCI\single_fit');
+    subjID = {'sub-MA01';'sub-MA08';'sub-MA53';'sub-MA98';...
+        'sub-MA103';'sub-MA104';'sub-MA109';'sub-MA114';...
+        'sub-MA115';'sub-MA117';'sub-MA122';...
+        'sub-MA124';'sub-MA125';'sub-MA126';'sub-MA129';'sub-MA130';...
+        'sub-MA132';'sub-MA134';'sub-MA137';'sub-MA138';...
+        'sub-MA142';'sub-MA145';'sub-MA146';'sub-MA147';...
+        'sub-MA149';'sub-MA151';'sub-MA152'};
+end
+
+load('group_bciSimulations_attmod_best10','group_best_results_mod');
+bci_att = group_best_results_mod;
+clear group_best_results_mod
+load('group_fusSimulations_attmod_best10','group_best_results_mod');
+fus_att = group_best_results_mod;
+clear group_best_results_mod
+load('group_fusSimulations_repmod_best10','group_best_results_repmod');
+fus_rep = group_best_results_repmod;
+clear group_best_results_repmod
+load('group_fusSimulations_attmod_switch_best10','group_best_results_mod_switch');
+fus_attrep = group_best_results_mod_switch;
+clear group_best_results_mod_switch
+
+logLike_null = nan(length(subjID),1);
+logLike_bci_att = nan(length(subjID),1);
+logLike_bci_noatt = nan(length(subjID),1);
+logLike_fus_att = nan(length(subjID),1);
+logLike_fus_noatt = nan(length(subjID),1);
+logLike_fus_rep = nan(length(subjID),1);
+logLike_fus_attrep = nan(length(subjID),1);
+
+R2 = nan(length(subjID),4);
+maxR2 = nan(length(subjID),1);
+
+for iSubj = 1:length(subjID)
+    
+    %% logLike for null model
+    if strcmp(expID, 'MRI')
+        cd(fullfile('E:\AMBRA\UoB\Data\MAMSI_MRI', subjID{iSubj}, 'behav\scanner'));
+    elseif strcmp(expID, 'behav')
+        cd(fullfile('E:\AMBRA\UoB\Data\MAMSI_MRI', 'MAMSI_MRI_behav', subjID{iSubj}));
+    end
+    
+    origData = dir('*Exp_All_Sessions*.mat');
+    load(origData.name, 'tdata');
+    origData = tdata;
+    
+    % Reorganize dataset 'tdata' so that I get the information I need for model estimation (locA, locV, respA, respV)
+    % Get rid of incorrect responses (wrong keypad), missed responses (no answer provided) and anticipated responses (RT < 100ms)
+    origData = origData(origData.IncorResp==0 & origData.MissedResp==0 & origData.AntResp==0,:);
+    
+    % Put locA and locV
+    if ~sum(strcmp(origData.Properties.VarNames(:),'locA')) % if locA is not present (and locV as well)
+        origData.locA = NaN(size(origData,1),1);
+        origData.locV = NaN(size(origData,1),1);
+        for j = 1:size(origData,1)
+            % locA: when people have to locate A, locA = location of the target and locV = location of the distractor
+            if strcmp(origData.ResponseModality(j), 'Aud')
+                origData.locA(j) = origData.TargetLoc(j);
+                origData.locV(j) = origData.NonTargetLoc(j);
+                % locV: when people have to locate V, locV = location of the target and locA = location of the distractor
+            elseif strcmp(origData.ResponseModality(j), 'Vis')
+                origData.locV(j) = origData.TargetLoc(j);
+                origData.locA(j) = origData.NonTargetLoc(j);
+            end
+        end
+    end
+    
+    % Put respA and respV
+    if ~sum(strcmp(origData.Properties.VarNames(:),'respA')) % if locA is not present (and respV as well)
+        origData.respA = NaN(size(origData,1),1);
+        origData.respV = NaN(size(origData,1),1);
+        for j = 1:size(origData,1)
+            % respA: when people have to locate A, respA = response
+            if strcmp(origData.ResponseModality(j), 'Aud')
+                origData.respA(j) = origData.Response(j);
+                origData.respV(j) = NaN;
+                % respV: when people have to locate V, respV = response
+            elseif strcmp(origData.ResponseModality(j), 'Vis')
+                origData.respV(j) = origData.Response(j);
+                origData.respA(j) = NaN;
+            end
+        end
+    end
+    
+    % Keep only useful data (locV, locA, respV, respA)
+    bciData = origData(:,{'locV','locA','respV','respA'});
+    actDataVA = dataset2table(bciData);
+    responseLoc = unique(bciData.locV);
+    
+    logLike_null(iSubj) = fitModelNull(actDataVA,responseLoc);
+    
+    %% logLike other models
+    
+    % BCI model no attention
+    if strcmp(expID, 'MRI')
+        file = fullfile(fullfile('E:\AMBRA\UoB\Data\MAMSI_MRI', subjID{iSubj}, 'behav\scanner', [subjID{iSubj} '_bciSimulations_best10']));
+    elseif strcmp(expID, 'behav') || strcmp(expID, 'behav_MRI')
+        file = fullfile(fullfile('E:\AMBRA\UoB\Data\MAMSI_MRI', 'MAMSI_MRI_behav', subjID{iSubj}, [subjID{iSubj} '_bciSimulations_best10']));
+    end
+    
+    load(file,'fm_bestlogLike');
+    logLike_bci_noatt(iSubj) = fm_bestlogLike;
+    clear fm_bestlogLike
+    
+    % fusion model no attention
+    if strcmp(expID, 'MRI')
+        file = fullfile(fullfile('E:\AMBRA\UoB\Data\MAMSI_MRI', subjID{iSubj}, 'behav\scanner', [subjID{iSubj} '_fusSimulations_best10']));
+    elseif strcmp(expID, 'behav') || strcmp(expID, 'behav_MRI')
+        file = fullfile(fullfile('E:\AMBRA\UoB\Data\MAMSI_MRI', 'MAMSI_MRI_behav', subjID{iSubj}, [subjID{iSubj} '_fusSimulations_best10']));
+    end
+    
+    load(file,'fm_bestlogLike');
+    logLike_fus_noatt(iSubj) = fm_bestlogLike;
+    clear fm_bestlogLike
+    
+    logLike_bci_att(iSubj) = bci_att{iSubj}.fmin_bestlogLike;
+    logLike_fus_att(iSubj) = fus_att{iSubj}.fmin_bestlogLike;
+    logLike_fus_rep(iSubj) = fus_rep{iSubj}.fmin_bestlogLike;
+    logLike_fus_attrep(iSubj) = fus_attrep{iSubj}.fmin_bestlogLike;
+    
+    %% Compute R2 for discrete responses based on Nagelkerke (1991)
+    n = size(bciData,1);
+    maxR2(iSubj,1) = 1-exp((2/n)*(-logLike_null(iSubj)));
+    
+    % we invert logLike_null and logLike_modelX because we have negative logLike
+    R2(iSubj,1) = 1-exp((-2/n)*(logLike_null(iSubj)-logLike_fus_noatt(iSubj)));
+    R2(iSubj,2) = 1-exp((-2/n)*(logLike_null(iSubj)-logLike_fus_att(iSubj)));
+    R2(iSubj,3) = 1-exp((-2/n)*(logLike_null(iSubj)-logLike_fus_rep(iSubj)));
+    R2(iSubj,4) = 1-exp((-2/n)*(logLike_null(iSubj)-logLike_fus_attrep(iSubj)));
+    R2(iSubj,5) = 1-exp((-2/n)*(logLike_null(iSubj)-logLike_bci_noatt(iSubj)));
+    R2(iSubj,6) = 1-exp((-2/n)*(logLike_null(iSubj)-logLike_bci_att(iSubj)));    
+    
+    R2(iSubj,1) = R2(iSubj,1)/maxR2(iSubj,1);
+    R2(iSubj,2) = R2(iSubj,2)/maxR2(iSubj,1);
+    R2(iSubj,3) = R2(iSubj,3)/maxR2(iSubj,1);
+    R2(iSubj,4) = R2(iSubj,4)/maxR2(iSubj,1);
+    R2(iSubj,5) = R2(iSubj,5)/maxR2(iSubj,1);
+    R2(iSubj,6) = R2(iSubj,6)/maxR2(iSubj,1);
+    
+end % End of loop over subjects
+
+meanR2 = mean(R2);
+semR2 = std(R2)/sqrt(length(subjID));
+
+if strcmp(expID, 'MRI')
+    save('E:\AMBRA\UoB\Data\MAMSI_MRI\group\behav\BCI\single_fit\R2.mat','R2');
+elseif strcmp(expID, 'behav')
+    save('E:\AMBRA\UoB\Data\MAMSI_MRI\group\behav\screening\BCI\single_fit\R2.mat','R2');
+end
+
---
+++ bci/model_comparison_rfx_ffx_bci_fus_att_rep.m
@@ -0,0 +1,135 @@
+%% RFX and FFX analysis on BIC
+% 1. BCI attention 2. BCI no attention
+% 3. fusion attention 4. fusion no attention
+% 5. fusion (report) no attention 6. fusion (report) attention
+
+clear;
+close all;
+clc;
+
+addpath(genpath('E:\AMBRA\UoB\Programs\spm12'));
+
+expID = 'MRI'; % MRI, behav, behav_MRI
+
+% We are in the data directory
+rootPath = pwd;
+
+if strcmp(expID, 'MRI')
+    cd('E:\AMBRA\UoB\Data\MAMSI_MRI\group\behav\BCI\single_fit');
+elseif strcmp(expID, 'behav')
+    cd('E:\AMBRA\UoB\Data\MAMSI_MRI\group\behav\screening\BCI\single_fit');
+elseif strcmp(expID, 'behav_MRI')
+    cd('E:\AMBRA\UoB\Data\MAMSI_MRI\group\behav\screening\MRI_subj\BCI\single_fit');
+end
+
+load('group_bci_aic_bic_attmod.mat')
+bci_att_bic_group = bic_group_mod;
+load('group_aic_bic.mat')
+bci_noatt_bic_group = bic_group_null;
+load('group_fus_aic_bic_attmod.mat')
+fus_att_bic_group = bic_group_mod;
+load('group_fus_aic_bic.mat')
+fus_noatt_bic_group = bic_group_null;
+load('group_fus_aic_bic_repmod.mat')
+fus_rep_bic_group = bic_group_repmod;
+load('group_fus_aic_bic_mod_switch.mat')
+fus_attrep_bic_group = bic_group_mod_switch;
+subjList = numel(bic_group_null);
+
+%% Create matrix for spm rfx (cols = 6 models, row = 12 subjects)
+
+rfx_bciA_bcinoA_fusA_fusnoA_fusR_fusRA = zeros(subjList,6);
+%rfx_fusatt_fusnoatt = zeros(subjList,2);
+rfx_results = cell(1,1); % 1 comparison
+
+% Comparison 1:
+% col 1: bci_att
+rfx_bciA_bcinoA_fusA_fusnoA_fusR_fusRA(:,1) = bci_att_bic_group;
+% col 2: bci_noatt
+rfx_bciA_bcinoA_fusA_fusnoA_fusR_fusRA(:,2) = bci_noatt_bic_group;
+% col 3: fus_att
+rfx_bciA_bcinoA_fusA_fusnoA_fusR_fusRA(:,3) = fus_att_bic_group;
+% col 4: fus_noatt
+rfx_bciA_bcinoA_fusA_fusnoA_fusR_fusRA(:,4) = fus_noatt_bic_group;
+% col 5: fus_rep
+rfx_bciA_bcinoA_fusA_fusnoA_fusR_fusRA(:,5) = fus_rep_bic_group;
+% col 6: fus_repatt
+rfx_bciA_bcinoA_fusA_fusnoA_fusR_fusRA(:,6) = fus_attrep_bic_group;
+
+BIC_group=rfx_bciA_bcinoA_fusA_fusnoA_fusR_fusRA(:,[4 3 5 6 2 1]);
+save('BIC.mat','BIC_group');
+
+[alpha,exp_r,xp,pxp,bor] = spm_BMS(rfx_bciA_bcinoA_fusA_fusnoA_fusR_fusRA);
+rfx_results{1} = [alpha,exp_r,xp,pxp,bor]; % 3 x alpha + 3 x exp_r + 3 x xp + 3 x pxp + bor
+
+fig1=figure('Position', [0 0 150 200]);
+bar(pxp(1:4),'FaceColor',[166 166 166]/255,'LineWidth',1.2);
+set(gca,'TickLength', [0.02 0.02]);
+set(gca,'LineWidth',1.2);
+xl = [0.2 3.8]; xlim(xl);
+set(gca, 'XTick', []);
+set(gca, 'XTickLabel', []);
+box off
+
+pxp_toplot = [pxp(4) pxp(3); pxp(5) pxp(6); pxp(2) pxp(1);];
+map_vector=0:0.01:1;
+map=repmat(map_vector',1,3);
+positionXY = [0, 0, 240, 240];
+figure('Position', positionXY);
+imagesc(pxp_toplot);
+colormap(map);
+colorbar;
+set(gca,'XTick',[1 2 3],...
+    'YTick',[1 2 3],...
+    'FontName', 'Helvetica', ...
+    'TickLength', [0 0], ...
+    'YTickLabel',[], ...
+    'XTickLabel',[]);
+
+saveas(gcf, 'pxp_bci_fus_mod_rep_plot_av09_MRI', 'emf');
+
+% % Comparison 2:
+% % col 1: fus_att
+% rfx_fusatt_fusnoatt(:,1) = fus_att_bic_group;
+% % col 2: fus_noatt
+% rfx_fusatt_fusnoatt(:,2) = fus_noatt_bic_group;
+% 
+% [alpha,exp_r,xp,pxp,bor] = spm_BMS(rfx_fusatt_fusnoatt);
+% rfx_results{2} = [alpha,exp_r,xp,pxp,bor]; % 3 x alpha + 3 x exp_r + 3 x xp + 3 x pxp + bor
+
+%% Create matrix for ffx (cols = 6 models, row = 12 subjects)
+
+ffx_comp = zeros(subjList,3);
+ffx_comp(:,1) = bci_att_bic_group-fus_noatt_bic_group;
+ffx_comp(:,2) = bci_att_bic_group-fus_att_bic_group;
+ffx_comp(:,3) = bci_att_bic_group-fus_rep_bic_group;
+ffx_comp(:,4) = bci_att_bic_group-fus_attrep_bic_group;
+ffx_comp(:,5) = bci_att_bic_group-bci_noatt_bic_group;
+
+fig2=figure;
+line([0 13],[0 0], 'Color','k','LineStyle', '-');
+hold on;
+plot(1:subjList, ffx_comp(:,1),'g*-');
+hold on;
+plot(1:subjList, ffx_comp(:,2),'b*-');
+hold on;
+plot(1:subjList, ffx_comp(:,3),'r*-');
+hold on;
+plot(1:subjList, ffx_comp(:,4),'c*-');
+hold on;
+plot(1:subjList, ffx_comp(:,5),'k*-');
+xlim([0 13]);
+set(gca, 'XTick', (1:1:12));
+set(gca, 'XTickLabel', (1:1:12));
+set(gca,'FontName', 'Helvetica');
+set(gca,'FontSize', 12);
+
+% ffx
+groupBIC = sum(ffx_comp);
+
+%% Save
+
+cd('E:\AMBRA\UoB\Data\MAMSI_MRI\group\behav\BCI');
+saveas(fig1, 'group_pxp_6mod_plot_av09_MRI', 'emf');
+saveas(fig2, 'group_bicREL_6mod_plot_av09_MRI', 'emf');
+save('rfx_ffx_6mod.mat','rfx_bciA_bcinoA_fusA_fusnoA_fusR_fusRA', 'rfx_results', 'ffx_comp');
